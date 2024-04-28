037a6972e4eb9cd8f59ad2aea6060298b64ae496
Kasia <kasia1781@gmail.com>
2024-04-28 18:39:54 +0200

    created NewTask component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,10 @@
+import NewTask from './NewTesk';
+
 export default function Task() {
 	return (
 		<section>
 			<h2 className='text-2xl font-bold text-stone-700 mb-4'>New Task</h2>
+			<NewTask />
 			<p className='text-stone-800 my-4'>
 				This project does not have any tasks yet.
 			</p>
